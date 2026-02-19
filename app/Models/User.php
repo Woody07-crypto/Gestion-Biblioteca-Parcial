7999b079144a0a1372c4a01dc22339310768936e
Dabesito <ehlerman13579@gmail.com>
2026-02-19 08:50:09 -0600

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,11 +23,9 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
+     
+      @var list<string>
+     
     protected $hidden = [
         'password',
         'remember_token',
@@ -37,7 +35,7 @@ class User extends Authenticatable
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
-     */
+        */
     protected function casts(): array
     {
         return [
