b8a4562e63bdf804d2fbd2d2998ae32848e511f1
Dabesito <ehlerman13579@gmail.com>
2026-02-19 08:24:48 -0600

    Ruta

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 use App\Http\Controllers\BookLoanRequestController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/book-loan-requests', [BookLoanRequestController::class, 'index']);
+
 Route::post('/book-loan-request', [BookLoanRequestController::class, 'store']);
 
